1d6face9adf1d8bef43ab3e62871e0d12ece2726
Elf <a.blake.mail@gmail.com>
2021-03-29 15:58:20 +0300

    28:41

--- lib/screens/details/body.dart
+++ lib/screens/details/body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/constants.dart';
 import 'package:shop_app/models/product.dart';
 import 'package:shop_app/screens/details/components/cart_counter.dart';
 import 'package:shop_app/screens/details/components/color_and_size.dart';
+import 'package:shop_app/screens/details/components/counter_with_fav_btn.dart';
 import 'package:shop_app/screens/details/components/description.dart';
 import 'package:shop_app/screens/details/components/product_title_with_image.dart';
 
@@ -28,7 +30,7 @@ class Body extends StatelessWidget {
                       top: size.height * 0.12,
                       left: kDefaultPadding,
                       right: kDefaultPadding),
-                  // height: 500,
+                  //height: 2000,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
@@ -40,13 +42,27 @@ class Body extends StatelessWidget {
                     children: [
                       ColorAndSize(product: product),
                       Description(product: product),
-                      Row(
-                        children: [
-                          CartCounter(),
-                          Container(
-                            height: 32,
-                          ),
-                        ],
+                      CounterWithFavButton(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: kDefaultPadding),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 58,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                                border: Border.all(color: product.color),
+                              ),
+                              child: IconButton(
+                                icon: SvgPicture.asset(
+                                    'assets/icons/add_to_cart.svg'),
+                                onPressed: () {},
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
